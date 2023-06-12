73bdb128c8a97c49a5c7862e0e1ba9f84ea6c672
Roland Panzou <panzouroland@gmail.com>
2023-06-12 13:51:12 +0200

    DevBMod

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -3,7 +3,9 @@ package com.m2i.tp;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("Lundi 12 Juin 2023 12h17");	
+		System.out.println("Lundi 12 Juin 2023 12h17");
+		System.out.println("Bientôt le déjeuner");
+		System.out.println("DevB is the best !");	
 
 	}
 
