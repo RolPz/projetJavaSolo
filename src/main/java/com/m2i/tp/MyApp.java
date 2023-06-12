32eb1e659c80bec719e7cf0792f05fbac979ed47
Roland Panzou <panzouroland@gmail.com>
2023-06-12 12:18:09 +0200

    modification heure

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -3,7 +3,7 @@ package com.m2i.tp;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("Lundi 12 Juin 2023 12h12");	
+		System.out.println("Lundi 12 Juin 2023 12h17");	
 
 	}
 
