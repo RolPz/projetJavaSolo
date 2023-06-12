e17b1989f6450e51da0b23750943bda58614ec90
Roland Panzou <panzouroland@gmail.com>
2023-06-12 14:14:02 +0200

    ModifAAA

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		System.out.println("Lundi 12 Juin 2023 12h17");
 		System.out.println("Bientôt le déjeuner");
-		System.out.println("DevAA is the best of all time!");	
+		System.out.println("DevAAA is the best of all time!");	
 
 	}
 
